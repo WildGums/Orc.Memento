ef963477ba7dc1772af2feef65365eb3cf093a5b
Geert van Horrik <geert@catenalogic.com>
2026-04-08 09:26:42 +0200

    Convert to file scoped namespaces

--- src/Orc.Memento.Tests/Helpers/ServiceCollectionHelper.cs
+++ src/Orc.Memento.Tests/Helpers/ServiceCollectionHelper.cs
@@ -1,20 +1,19 @@
-﻿namespace Orc.Memento.Tests
+﻿namespace Orc.Memento.Tests;
+
+using Catel;
+using Microsoft.Extensions.DependencyInjection;
+using Orc.Memento;
+
+internal static class ServiceCollectionHelper
 {
-    using Catel;
-    using Microsoft.Extensions.DependencyInjection;
-    using Orc.Memento;
-
-    internal static class ServiceCollectionHelper
+    public static IServiceCollection CreateServiceCollection()
     {
-        public static IServiceCollection CreateServiceCollection()
-        {
-            var serviceCollection = new ServiceCollection();
+        var serviceCollection = new ServiceCollection();
 
-            serviceCollection.AddLogging();
-            serviceCollection.AddCatelCore();
-            serviceCollection.AddOrcMemento();
+        serviceCollection.AddLogging();
+        serviceCollection.AddCatelCore();
+        serviceCollection.AddOrcMemento();
 
-            return serviceCollection;
-        }
+        return serviceCollection;
     }
 }
